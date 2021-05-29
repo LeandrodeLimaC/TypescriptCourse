6be82d553cb24f32d7516905d76ab5cde24fe2fd
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-05-28 21:37:40 -0300

    Done Section 7: Interfaces

--- interfaces/interfaces.js
+++ interfaces/interfaces.js
@@ -56,4 +56,20 @@ class AbstrataABD {
     a() { }
     b() { }
 }
+Object.prototype.log = function () {
+    console.log(this.toString());
+};
+const x = 2;
+const y = 3;
+const z = 4;
+x.log();
+y.log();
+z.log();
+const cli = {
+    nome: 'Pedro',
+    toString() {
+        return this.nome;
+    }
+};
+cli.log();
 //# sourceMappingURL=interfaces.js.map
--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -93,3 +93,27 @@ abstract class AbstrataABD implements A, B {
     abstract d(): void
 }
 
+interface Object {
+    log(): void
+}
+
+Object.prototype.log = function () {
+    console.log(this.toString())
+}
+
+const x = 2
+const y = 3
+const z = 4
+
+x.log()
+y.log()
+z.log()
+
+const cli = {
+    nome: 'Pedro',
+    toString() {
+        return this.nome
+    }
+}
+
+cli.log()
