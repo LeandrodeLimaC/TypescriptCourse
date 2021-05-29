347de639708931efda717f0a6d3b3e33bdb7aaa4
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-05-28 21:18:48 -0300

    Functions on interfaces& class with Interfaces

--- interfaces/interfaces.js
+++ interfaces/interfaces.js
@@ -7,10 +7,53 @@ function mudarNome(pessoa) {
 }
 const pessoa = {
     nome: 'João',
-    idade: 27
+    idade: 27,
+    saudar(sobrenome) {
+        console.log('Olá, meu nome é '
+            + this.nome + ' ' + sobrenome);
+    }
 };
 saudarComOla(pessoa);
 mudarNome(pessoa);
 saudarComOla(pessoa);
-saudarComOla({ nome: 'Jonas', idade: 27 });
+// saudarComOla({ nome: 'Jonas', idade: 27, altura: 1.75 })
+pessoa.saudar('Skywalker');
+// Usando Classes...
+class Cliente {
+    constructor() {
+        this.nome = '';
+        this.ultimaCompra = new Date;
+    }
+    saudar(sobrenome) {
+        console.log('Olá, meu nome é '
+            + this.nome + ' ' + sobrenome);
+    }
+}
+const meuCliente = new Cliente();
+meuCliente.nome = 'Han';
+saudarComOla(meuCliente);
+meuCliente.saudar('Solo');
+console.log(meuCliente.ultimaCompra);
+let potencia;
+potencia = (base, exp) => {
+    return Math.pow(base, exp);
+};
+console.log(Math.pow(3, 2));
+console.log(potencia(3, 2));
+class RealA {
+    a() { }
+}
+class RealAB {
+    a() { }
+    b() { }
+}
+class RealABC {
+    a() { }
+    b() { }
+    c() { }
+}
+class AbstrataABD {
+    a() { }
+    b() { }
+}
 //# sourceMappingURL=interfaces.js.map
--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -1,5 +1,9 @@
 interface Humano {
-    nome: string
+    nome: string,
+    idade?: number,
+    [prop: string]: any
+
+    saudar(sobrenome: string): void
 }
 
 function saudarComOla(pessoa: Humano) {
@@ -10,9 +14,13 @@ function mudarNome(pessoa: Humano) {
     pessoa.nome = 'Joana'
 }
 
-const pessoa = {
+const pessoa: Humano = {
     nome: 'João',
-    idade: 27
+    idade: 27,
+    saudar(sobrenome: string): void {
+        console.log('Olá, meu nome é '
+            + this.nome + ' ' + sobrenome)
+    }
 }
 
 saudarComOla(pessoa)
@@ -20,4 +28,68 @@ saudarComOla(pessoa)
 mudarNome(pessoa)
 saudarComOla(pessoa)
 
-saudarComOla({ nome: 'Jonas', idade: 27 })
+// saudarComOla({ nome: 'Jonas', idade: 27, altura: 1.75 })
+pessoa.saudar('Skywalker')
+
+// Usando Classes...
+class Cliente implements Humano {
+    nome: string = ''
+    ultimaCompra: Date = new Date
+
+    saudar(sobrenome: string): void {
+        console.log('Olá, meu nome é '
+            + this.nome + ' ' + sobrenome)
+    }
+}
+
+const meuCliente = new Cliente()
+meuCliente.nome = 'Han'
+saudarComOla(meuCliente)
+meuCliente.saudar('Solo')
+console.log(meuCliente.ultimaCompra)
+
+// Interface Função 
+interface FuncaoCalculo {
+    (a: number, b: number): number
+}
+
+let potencia: FuncaoCalculo
+
+potencia = (base: number, exp: number): number => {
+    return base ** exp
+}
+
+console.log(Math.pow(3, 2))
+console.log(potencia(3, 2))
+
+// Herança
+interface A {
+    a(): void
+}
+interface B {
+    b(): void
+}
+
+interface ABC extends A, B {
+    c(): void
+}
+
+class RealA implements A {
+    a(): void { }
+}
+class RealAB implements A, B {
+    a(): void { }
+    b(): void { }
+}
+class RealABC implements ABC {
+    a(): void { }
+    b(): void { }
+    c(): void { }
+}
+
+abstract class AbstrataABD implements A, B {
+    a(): void { }
+    b(): void { }
+    abstract d(): void
+}
+
