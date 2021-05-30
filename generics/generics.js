c6755d011bdf03e39ba7601e5af9d413d873ee66
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-05-30 12:29:25 -0300

    Challenge Fila class

--- generics/generics.js
+++ generics/generics.js
@@ -82,4 +82,47 @@ class DiferencaEntreDatas extends OperacaoBinaria {
 const d1 = new Data(1, 2, 2020);
 const d2 = new Data(10, 2, 2020);
 console.log(new DiferencaEntreDatas(d1, d2).executar());
+// Desafio Classe Fila
+// Atributo: fila (Array)
+// Métodos: entrar, próximo, imprimir
+// class Fila<T> {
+//     constructor(
+//         private fila: T[] = []
+//     ) { }
+//     entrar(item: T) {
+//         this.fila.push(item)
+//     }
+//     proximo() {
+//         this.fila.splice(0, 1)
+//     }
+//     imprimir(): void {
+//         console.log(this.fila)
+//     }
+// }
+class Fila {
+    constructor(...args) {
+        this.fila = args;
+    }
+    entrar(elemento) {
+        this.fila.push(elemento);
+    }
+    proximo() {
+        if (!this.fila[0])
+            return null;
+        const primeiro = this.fila[0];
+        this.fila.splice(0, 1);
+        return primeiro;
+    }
+    imprimir() {
+        console.log(this.fila);
+    }
+}
+const fila = new Fila('Gui', 'Pedro', 'Ana', 'Lu');
+fila.imprimir();
+fila.entrar('Rafael');
+fila.imprimir();
+console.log(fila.proximo());
+console.log(fila.proximo());
+console.log(fila.proximo());
+fila.imprimir();
 //# sourceMappingURL=generics.js.map
--- generics/generics.ts
+++ generics/generics.ts
@@ -107,3 +107,59 @@ const d1 = new Data(1, 2, 2020)
 const d2 = new Data(10, 2, 2020)
 
 console.log(new DiferencaEntreDatas(d1, d2).executar())
+
+// Desafio Classe Fila
+// Atributo: fila (Array)
+// Métodos: entrar, próximo, imprimir
+
+// class Fila<T> {
+//     constructor(
+//         private fila: T[] = []
+//     ) { }
+
+//     entrar(item: T) {
+//         this.fila.push(item)
+//     }
+
+//     proximo() {
+//         this.fila.splice(0, 1)
+//     }
+
+//     imprimir(): void {
+//         console.log(this.fila)
+//     }
+// }
+class Fila<T> {
+    private fila: Array<T>
+
+    constructor(...args: T[]) {
+        this.fila = args
+    }
+
+    entrar(elemento: T) {
+        this.fila.push(elemento)
+    }
+
+    proximo(): T | null {
+        if (!this.fila[0])
+            return null;
+
+        const primeiro = this.fila[0]
+        this.fila.splice(0, 1)
+        return primeiro
+    }
+
+    imprimir(): void {
+        console.log(this.fila)
+    }
+}
+
+const fila = new Fila<string>('Gui', 'Pedro', 'Ana', 'Lu')
+fila.imprimir()
+fila.entrar('Rafael')
+fila.imprimir()
+console.log(fila.proximo())
+console.log(fila.proximo())
+console.log(fila.proximo())
+fila.imprimir()
+
