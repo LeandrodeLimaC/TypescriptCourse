7d0027f6a31a2ef736c560819ee786aaf01e2a9c
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-05-30 11:43:26 -0300

    Real world example of abstract class being extended by a class with generics

--- generics/generics.js
+++ generics/generics.js
@@ -36,4 +36,50 @@ imprimir([
     { nome: 'Cicrano', idade: 23 },
     { nome: 'Beltrano', idade: 24 },
 ]);
+const chamarEcho = echoMelhorado;
+console.log(chamarEcho('João'));
+// Class com Generics
+// class OperacaoBinaria {
+//     constructor(
+//         public operando1: any,
+//         public operando2: any,
+//     ) { }
+//     executar() {
+//         return this.operando1 + this.operando2;
+//     }
+// }
+class OperacaoBinaria {
+    constructor(operando1, operando2) {
+        this.operando1 = operando1;
+        this.operando2 = operando2;
+    }
+}
+// console.log(new OperacaoBinaria('Bom', 'dia').executar())
+// console.log(new OperacaoBinaria(3, 7).executar())
+// console.log(new OperacaoBinaria(3, 'Opa').executar())
+// console.log(new OperacaoBinaria({}, {}).executar())
+class SomaBinaria extends OperacaoBinaria {
+    executar() {
+        return this.operando1 + this.operando2;
+    }
+}
+console.log('SomaBinaria: ', new SomaBinaria(3, 4).executar());
+// DESAFIO NÃO PLANEJADO 
+class DiferencaEntreDatas extends OperacaoBinaria {
+    getTime(data) {
+        let { dia, mes, ano } = data;
+        // return new Date(`${mes}/${dia}/${ano}`).getTime()
+        return new Date(ano, mes, dia).getTime();
+    }
+    executar() {
+        const t1 = this.getTime(this.operando1);
+        const t2 = this.getTime(this.operando2);
+        const diferenca = Math.abs(t1 - t2);
+        const dia = 1000 * 60 * 60 * 24;
+        return `${Math.ceil(diferenca / dia)} dia(s)`;
+    }
+}
+const d1 = new Data(1, 2, 2020);
+const d2 = new Data(10, 2, 2020);
+console.log(new DiferencaEntreDatas(d1, d2).executar());
 //# sourceMappingURL=generics.js.map
--- generics/generics.ts
+++ generics/generics.ts
@@ -44,3 +44,66 @@ imprimir<Aluno>([
     { nome: 'Cicrano', idade: 23 },
     { nome: 'Beltrano', idade: 24 },
 ])
+
+// Tipo Genérico
+type Echo = <T>(data: T) => T
+const chamarEcho: Echo = echoMelhorado;
+console.log(chamarEcho<string>('João'));
+
+// Class com Generics
+// class OperacaoBinaria {
+//     constructor(
+//         public operando1: any,
+//         public operando2: any,
+//     ) { }
+
+//     executar() {
+//         return this.operando1 + this.operando2;
+//     }
+// }
+abstract class OperacaoBinaria<T, R> {
+    constructor(
+        public operando1: T,
+        public operando2: T,
+    ) { }
+
+    abstract executar(): R
+}
+
+// console.log(new OperacaoBinaria('Bom', 'dia').executar())
+// console.log(new OperacaoBinaria(3, 7).executar())
+// console.log(new OperacaoBinaria(3, 'Opa').executar())
+// console.log(new OperacaoBinaria({}, {}).executar())
+
+class SomaBinaria extends OperacaoBinaria<number, number> {
+    executar(): number {
+        return this.operando1 + this.operando2
+    }
+}
+
+console.log('SomaBinaria: ', new SomaBinaria(3, 4).executar())
+
+// DESAFIO NÃO PLANEJADO 
+class DiferencaEntreDatas
+    extends OperacaoBinaria<Data, string> {
+
+    getTime(data: Data): number {
+        let { dia, mes, ano } = data;
+
+        // return new Date(`${mes}/${dia}/${ano}`).getTime()
+        return new Date(ano, mes, dia).getTime()
+    }
+
+    executar(): string {
+        const t1 = this.getTime(this.operando1)
+        const t2 = this.getTime(this.operando2)
+        const diferenca = Math.abs(t1 - t2)
+        const dia = 1000 * 60 * 60 * 24
+        return `${Math.ceil(diferenca / dia)} dia(s)`
+    }
+}
+
+const d1 = new Data(1, 2, 2020)
+const d2 = new Data(10, 2, 2020)
+
+console.log(new DiferencaEntreDatas(d1, d2).executar())
